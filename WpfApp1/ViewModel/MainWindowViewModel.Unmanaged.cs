823c0928737701c405486f9d5a44ddb4f619324b
Adam Fierek <31532851+kolorowezworki@users.noreply.github.com>
2023-06-18 10:39:23 +0200

    Test P/Invoke, moved Measurement template to separate xaml file

---
+++ WpfApp1/ViewModel/MainWindowViewModel.Unmanaged.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WpfApp1.ViewModel
+{
+    public partial class MainWindowViewModel
+    {
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    }
+}
--- WpfApp1/ViewModel/MainWindowViewModel.cs
+++ WpfApp1/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Interop;
 using WpfApp.MVVM;
 using WpfApp1.Model;
 using WpfApp1.Service;
 
 namespace WpfApp1.ViewModel
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
         private MeasurementService _service;
         private Measurement lastMeasurement;
@@ -17,10 +21,26 @@ namespace WpfApp1.ViewModel
         {
             _service = new MeasurementService();
             GetMeasurementCommand = new Command(GetMeasurement);
+            RunPInvoke = new Command(RunPInvokeCommand);
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 1_000;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Trace.WriteLine("Tick!");
+        }
+
+        private void RunPInvokeCommand()
+        {
+            var hWindow = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+            MessageBox(hWindow, "Hello from P/Invoke", "Message", 0);
         }
 
         public Command GetMeasurementCommand { get; set; }
-
+        public Command RunPInvoke { get; private set; }
         public List<Measurement> Measurements { get => measurements; set => Set(ref measurements, value); }
 
         public Measurement LastMeasurement
